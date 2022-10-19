c0f124067ed1a248af31b31e2820b4bec2c6f082
Kaue Rodrigues <kaue.r.s@hotmail.com>
2022-10-19 01:08:26 -0300

    Aula Inicial Javascript

---
+++ autotest.js
@@ -0,0 +1,12 @@
+
+
+const meuNome = "Kauê";
+const minhaIdade = 2022 - 1999;
+const localDeNascimento = "São Vicente";
+
+//const minhaApresentacao = "Meu Nome é " + meuNome + ",minha idade é" + minhaIdade + "e nasci em" + localDeNascimento;
+
+
+const minhaApresentacao = `Meu Nome é ${meuNome}, minha idade é ${minhaIdade} e meu local de Nascimento é ${localDeNascimento}`
+
+console.log (minhaApresentacao)
